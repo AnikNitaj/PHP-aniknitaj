73c4bba8cb0abd713e98800d73496c43b6071320
AnikNitaj <aniknitaj1@gmail.com>
2025-11-10 10:10:26 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <!DOCTYPE html>
+    <body>
+        <h1>Welcome to My Website</h1>
+        <?php
+            echo "Hello, World!";
+            "SELECT * FROM Students";
+        ?>
+    </body>
+</html>

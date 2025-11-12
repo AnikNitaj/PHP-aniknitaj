67eecb1c4367b5bb4516bfa251e4e1f0f35f6eab
AnikNitaj <aniknitaj1@gmail.com>
2025-11-12 18:50:14 +0100

    moduli 2

---
+++ modul 2/index.php
@@ -0,0 +1,28 @@
+<?php
+
+//$school="Digital School";
+//var_dump($school);
+//echo $school;
+
+//$x=120;
+//$y=50;
+//echo $x+$y."<br>";
+//echo $x-$y."<br>";
+//echo $x/$y."<br>";
+//echo $x*$y."<br>";
+
+//$a="Digital";
+//$b="School";
+//echo $a.$b;
+
+
+$programming="Programing is Not fun";
+
+echo str_replace("not","very",$programming). "<br>";
+
+echo str_word_count($programming)."<br>";
+
+echo strlen($programming);
+
+echo strrev($programming);
+?>

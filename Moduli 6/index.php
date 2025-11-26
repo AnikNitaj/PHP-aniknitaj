e37bbbc706a853a47a1b12f969cd89816d74e211
AnikNitaj <aniknitaj1@gmail.com>
2025-11-26 10:01:59 -0800

    moduli 6

---
+++ Moduli 6/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// $my_file = fopen("ds.txt", "w");
+
+// fclose($my_file);
+
+// $my_files=fopen("dss.txt", "r");
+
+// $file=fopen("example.txt","r");
+
+// while(!feof($file)){
+//     echo fgets($file)."<br>";
+// }
+
+// fclose($file);
+
+// $my_file = fopen("ds.txt", "w");
+
+// $my_text = "Digital School\n";
+
+// fwrite($my_file, $my_text);
+
+$h=fopen("data.txt","w+");
+
+fwrite($h,"Text test 1");
+
+$handle=fopen("data.txt","a+");
+fwrite($handle,"add more lines to this file");
+fclose($handle);
+?>
+
+
